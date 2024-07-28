b188d27921e3ac275bf6f74f7b7a8cf8fd554e03
ohtsuka takahiro <t.o.0526@gmail.com>
2024-07-28 23:06:37 +0900

    alarm

--- app.vue
+++ app.vue
@@ -4,6 +4,9 @@ const second = ref(0);
 const count = ref(0);
 const status = ref('');
 let intervalId = '';
+const volume = ref(10);
+const alarm1 = ref(10);
+const alarm2 = ref(10);
 const onStartClick = (): void => {
   startTimer();
   status.value = 'running';
@@ -27,10 +30,20 @@ const startTimer = (): void => {
         minute.value += 1;
       } else {
         second.value += 1;
-      }
+      };
+      if (count.value == alarm1.value || count.value == alarm2.value) {
+        const audio = new Audio('alarm.mp3');
+        audio.volume = volume.value / 10.0;
+        audio.play();
+      };
     },
     1000
   );
+}
+const volumeTest = (): void => {
+  const audio = new Audio('alarm.mp3');
+  audio.volume = volume.value / 10.0;
+  audio.play();
 }
 </script>
 
@@ -49,5 +62,16 @@ const startTimer = (): void => {
     <button disabled v-if="status == ''">リセット</button>
     <button disabled v-else-if="status == 'running'">リセット</button>
     <button v-on:click='onResetClick' v-if="status == 'stop'">リセット</button>
+    <br />
+    音量(1~10)
+    <input v-model='volume'>
+    <br />
+    <button v-on:click='volumeTest'>音量テスト</button>
+    <br />
+    アラーム1(秒)
+    <input v-model='alarm1'>
+    <br />
+    アラーム2(秒)
+    <input v-model='alarm2'>
   </div>
 </template>
